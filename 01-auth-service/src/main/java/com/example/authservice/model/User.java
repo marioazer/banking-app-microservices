8b4f9d41ae52314399aaa290d5c8977a6953f3ad
Mario Azer <marioazeryt@gmail.com>
2026-07-22 14:51:31 -0400

    first 2 microservices tests pass

--- 01-auth-service/src/main/java/com/example/authservice/model/User.java
+++ 01-auth-service/src/main/java/com/example/authservice/model/User.java
@@ -1,5 +1,49 @@
 package com.example.authservice.model;
 
-public class User {
-    
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String phoneNumber;
+
+    private Boolean totpEnabled = false;
+    private String totpSecret;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
 }
